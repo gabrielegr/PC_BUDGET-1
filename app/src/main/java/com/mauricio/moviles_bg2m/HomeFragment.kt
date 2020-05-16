a515f9ea9f09273cce4c40cffabd73b06ce679e1
BryOrellana <00049318@uca.edu.sv>
2020-05-16 17:19:58 -0600

    Vistas presupuesto y menu de mauricio

---
+++ app/src/main/java/com/mauricio/moviles_bg2m/HomeFragment.kt
@@ -0,0 +1,38 @@
+package com.mauricio.moviles_bg2m
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.mauricio.moviles_bg2m.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(
+            inflater, R.layout.fragment_home, container,
+            false
+        )
+        setToolbarTitle()
+
+        return binding.root
+    }
+
+    private fun setToolbarTitle() {
+        (activity as AppCompatActivity)
+            .supportActionBar?.title = getString(
+            R.string.toolbar_title_home
+        )
+
+    }
+}
---
+++ app/src/main/java/com/mauricio/moviles_bg2m/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.mauricio.moviles_bg2m
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mauricio.moviles_bg2m.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        replaceFragment(HomeFragment())
+        colorToolbar()
+    }
+
+
+    private val mOnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_inicio -> {
+                    println("home pressed")
+                    replaceFragment(HomeFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.nav_producto -> {
+                    println("product pressed")
+                    replaceFragment(ProductsFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.nav_presupuesto -> {
+                    println("home pressed")
+                    replaceFragment(BudgetFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.nav_mas -> {
+                    println("home pressed")
+                    replaceFragment(MoreFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.Fragment_container, fragment)
+        fragmentTransaction.commit()
+    }
+
+
+    private fun colorToolbar(){
+        val actionBar: ActionBar?
+        actionBar = supportActionBar
+
+        val colorDrawable = ColorDrawable(Color.parseColor("#00303f"))
+
+        actionBar?.setBackgroundDrawable(colorDrawable);
+    }
+}
