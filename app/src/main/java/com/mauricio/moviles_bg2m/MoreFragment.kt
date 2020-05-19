2933d687369b36bbddd2c79c290462b0a20ed0c6
BryOrellana <00049318@uca.edu.sv>
2020-05-19 15:45:25 -0600

    Solucionando error xd

--- app/src/main/java/com/mauricio/moviles_bg2m/MoreFragment.kt
+++ app/src/main/java/com/mauricio/moviles_bg2m/MoreFragment.kt
@@ -1,21 +1,13 @@
 package com.mauricio.moviles_bg2m
 
-import android.app.AlertDialog
-import android.app.Dialog
-import android.app.ProgressDialog.show
-import android.content.DialogInterface
+
 import android.os.Bundle
-import android.os.Message
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.DialogFragment
-import com.google.android.material.snackbar.Snackbar
 import com.mauricio.moviles_bg2m.databinding.FragmentMoreBinding
 
 class MoreFragment : Fragment() {
