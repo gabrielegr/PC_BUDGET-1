d9d90d1bb34ce05aaf759e2ee2200af74c27f370
Mauricio2801 <00137118@uca.edu.sv>
2020-05-20 19:00:42 -0600

    Navegacion de la vista mas a la vista Cambiar contraseña

--- app/src/main/java/com/mauricio/moviles_bg2m/MoreFragment.kt
+++ app/src/main/java/com/mauricio/moviles_bg2m/MoreFragment.kt
@@ -1,6 +1,7 @@
 package com.mauricio.moviles_bg2m
 
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,7 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.mauricio.moviles_bg2m.databinding.FragmentMoreBinding
+import kotlinx.android.synthetic.main.fragment_more.*
 
 class MoreFragment : Fragment() {
     private lateinit var binding: FragmentMoreBinding
@@ -16,10 +19,18 @@ class MoreFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate<FragmentMoreBinding>(inflater, R.layout.fragment_more, container,false)
+        var binding = DataBindingUtil.inflate<FragmentMoreBinding>(inflater, R.layout.fragment_more, container,false)
 
         setToolbarTitle()
         return binding.root
+
+        binding.apply {
+            cambiar_contra.setOnClickListener { view: View ->
+                view.findNavController().navigate(R.id.action_moreFragment_to_cambiopasswordFragment)
+            }
+
+        }
+
     }
 
     private fun setToolbarTitle() {
@@ -29,4 +40,8 @@ class MoreFragment : Fragment() {
         )
 
     }
+
+
 }
+
+
